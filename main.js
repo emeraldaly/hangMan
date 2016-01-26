62ae8a05165185b26d0154eac43c75a24da95fb9
Alyson <emerald.aly@gmail.com>
2016-01-26 17:16:15 -0500

    cleaned up code alignment

--- main.js
+++ main.js
@@ -1,54 +1,55 @@
+
 var prompt = require('prompt');
 var Word = require('./word.js');
 
 prompt.start();
 
 var Game = {
-  wordBank: ["Taj Mahal", "Machu Picchu", "Colosseum", "Petra", "Great Wall", "Chichen Itza", "Christ Redeemer", "Great Pyramids", "Colossus of Rhodes", "Hanging Gardens of Babylong", "Lighthouse of Alexandria", "Mausoleum at Halicarnassus", "Statue of Zeus at Olympia", "Temple of Artemis at Ephesus"]
-  guessesRemaining: 10;
-  currentWrd: null;
+    wordBank: ["Taj Mahal", "Machu Picchu", "Colosseum", "Petra", "Great Wall", "Chichen Itza", "Christ Redeemer", "Great Pyramids", "Colossus of Rhodes", "Hanging Gardens of Babylong", "Lighthouse of Alexandria", "Mausoleum at Halicarnassus", "Statue of Zeus at Olympia", "Temple of Artemis at Ephesus"]
+    guessesRemaining: 10;
+    currentWrd: null;
 }
+debugger;
+startGame = function(wrd) {
+        this.currentWrd = new Word(this.wordBank[Math.floor(Math.random() * this.wordBank.length)]);
+        this.currentWrd.getLets();
+        this.keepPromptingUser();
 
-startGame = function(wrd){
-  this.currentWrd = new Word(this.wordBank[Math.floor(Math.random() * this.wordBank.length)]);
-  this.currentWrd.getLets();
-  this.keepPromptingUser();
+        console.log(currentWrd);
+    },
 
-  console.log(currentWrd);
-},
+    keepPromptingUser = function() {
+        var self = this;
 
-keepPromptingUser = function(){
-var self = this;
+        prompt.get(['guessLetter'], function(err, result) {
+            console.log("The Letter or Space You Guessed is" + result.guessLetter);
 
-prompt.get(['guessLetter'], function (err, result){
-console.log("The Letter or Space You Guessed is" + result.guessLetter);
+            var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
 
-var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
-
-if (findHowManyOfUserGuess === 0){
-console.log("You guess wrong :(")
-self.guessesRemaining --;
-}else{
-  console.log("You guessed right!");
+            if (findHowManyOfUserGuess === 0) {
+                console.log("You guess wrong :(")
+                self.guessesRemaining--;
+            } else {
+                console.log("You guessed right!");
 
 
-  if (self.currentWrd.didWeFindTheWord() === true){
-  console.log("You Won!");
-  return 1;
-  }else{
+                if (self.currentWrd.didWeFindTheWord() === true) {
+                    console.log("You Won!");
+                    return 1;
+                } else {
 
-    console.log ("Guesses remaining" + self.guessesRemaining);
-    console.log(self.currentWrd.wordRender());
-  }
-}
+                    console.log("Guesses remaining" + self.guessesRemaining);
+                    console.log(self.currentWrd.wordRender());
+                }
+            }
 
-if(self.guessesRemaining>0 && self.currentWrd.found === false){
-  self.keepPromptingUser();
-}else if(self.guessesRemaining === 0) {
-  console.log('Game Over :(');
-    console.log('The word was: ' + self.currentWrd.word);
-}
-});
-}
+            if (self.guessesRemaining > 0 && self.currentWrd.found === false) {
+                self.keepPromptingUser();
+            } else if (self.guessesRemaining === 0) {
+                console.log('Game Over :(');
+                console.log('The word was: ' + self.currentWrd.word);
+            }
+        });
+    }
 }
 game.startGame();
--- word.js
+++ word.js
@@ -1,32 +1,33 @@
+
 var Letter = require('./letter.js');
 
-var Word = function(wrd){
-  this.word = wrd;
-  this.lets = [];
-  this.found = false;
+var Word = function(wrd) {
+    this.word = wrd;
+    this.lets = [];
+    this.found = false;
 
-  this.getLets = function() {
-    for(var i = 0; i < this.word.length; i++) {
-      this.lets.push(new Letter(this.word[i]));
+    this.getLets = function() {
+        for (var i = 0; i < this.word.length; i++) {
+            this.lets.push(new Letter(this.word[i]));
+        }
+    };
+
+    this.didWeFindTheWord = function() {
+        this.found = this.lets.every(function(curLet) {
+            return curLet.appear;
+        });
+        return this.found;
+    };
+
+    this.wordRender = function() {
+        var str = "";
+        for (var i = 0; i < this.word.let; i++) {
+            this.letterRender.push(new str)
+            return this.str;
+        }
+
     }
-  };
-
-  this.didWeFindTheWord = function () {
-    this.found = this.lets.every(function(curLet) {
-      return curLet.appear;
-    });
-    return this.found;
-  };
-
-  this.wordRender = function (){
-    var str = "";
-    for(var i = 0; i < this.word.let; i++) {
-      this.letterRender.push(new str)
-      return this.str;
-    }
-
-  }
-  this.checkIfLetterFound = function(guessLetter)
+    this.checkIfLetterFound = function(guessLetter)
 }
 
 module.exports = Word;
